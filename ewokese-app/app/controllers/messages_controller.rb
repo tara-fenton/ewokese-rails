2ed242d6a3aa22b30853938f2c1ca54c64d3f838
Tara Fenton <tarafenton@ool-ad030165.dyn.optonline.net>
2019-01-07 15:16:44 -0500

    edit controllers

--- ewokese-app/app/controllers/messages_controller.rb
+++ ewokese-app/app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :update, :destroy]
+
+  # GET /messages
+  def index
+    @messages = Message.all
+
+    render json: @messages
+  end
+
+  # GET /messages/1
+  def show
+    render json: @message
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
 end
--- ewokese-app/app/controllers/users_controller.rb
+++ ewokese-app/app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+
+    render json: @user
+  end
+
+
   def new
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
