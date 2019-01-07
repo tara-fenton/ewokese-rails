5a0d1521c6a630f9efd7724ae66d716d3a271652
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-01-06 22:12:32 -0500

    seed data

--- ewokese-app/db/seeds.rb
+++ ewokese-app/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+User.destroy_all
+
+users = User.create([
+  {
+    email: 'a@a.co',
+    password_digest: 'aaaa'
+  },
+  {
+    email: 'b@b.co',
+    password_digest: 'bbbb'
+  },
+  {
+    email: 'c@c.co',
+    password_digest: 'cccc'
+  }
+])
+
+message = Message.create([
+  {
+    message: 'AAA'
+  },
+  {
+    message: 'BBB'
+  },
+  {
+    message: 'CCC'
+  }
+])
