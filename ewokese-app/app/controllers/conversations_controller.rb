4d11df4c97e2653c7d3d04185e8493b9fd7dd910
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-01-08 21:19:06 -0500

    conversations index

--- ewokese-app/app/controllers/conversations_controller.rb
+++ ewokese-app/app/controllers/conversations_controller.rb
@@ -1,10 +1,28 @@
 class ConversationsController < ApplicationController
+  before_action :set_conversation, only: [:show, :update, :destroy]
 
-  # GET /messages
+  # GET /conversations
   def index
     @conversations = Conversation.all
 
     render json: @conversations
   end
+
+  # GET /conversations/1
+  def show
+
+    render json: @conversation
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_conversation
+      @conversation = Conversation.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    # def user_params
+    #   params.require(:user).permit(:email, :password, :password_confirmation)
+    # end
 
 end
