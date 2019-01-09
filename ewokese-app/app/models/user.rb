061fc0ca7ad004cb13d328c078a6b9c39132d67e
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-01-08 21:47:40 -0500

    add to_token_payload

--- ewokese-app/app/models/user.rb
+++ ewokese-app/app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :conversations
+
+  has_secure_password
+    validates :email, presence: true
+
+    def to_token_payload
+        {
+            sub: id,
+            email: email
+        }
+    end
 end
