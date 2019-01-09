beac32040d20a9a4c95099ad5db06c95c66130ad
Tara Fenton <tarafenton@ool-ad030165.dyn.optonline.net>
2019-01-09 15:45:06 -0500

    message create, update, destroy

--- ewokese-app/app/controllers/messages_controller.rb
+++ ewokese-app/app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :update, :destroy]
+  before_action :authenticate_user, only: [:create, :update, :destroy]
 
   # GET /messages
   def index
@@ -13,10 +14,40 @@ class MessagesController < ApplicationController
     render json: @message
   end
 
+  # POST /messages
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render json: @message, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /messages/1
+  def update
+    if @message.update(message_params)
+      render json: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /messages/1
+  def destroy
+    @message.destroy
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def message_params
+      params.permit(:message)
     end
 
 end
